6f2a99ae9aaa62d01be623072bf54009a40907e5
Karen-Yuen <36660868+Karen-Yuen@users.noreply.github.com>
2018-02-23 17:34:28 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,15 +14,14 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <string>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <functional>
+#include <thread>
+#include <future>
 
 using namespace std;
-
+typedef function<void(int i)> CallBackFunc;
 /*
  * 
  */
@@ -41,46 +40,43 @@ void result (int argc, char* argv[])
         
 }
 
+void progressReport(int i){
+    
+    cout<< "\e[A"<< i+1 <<" byte is done";
+                cout << '\n';
+    
+}
 
-int copy (string source, string dest) // callback progressReport
+int copy (string source, string dest, CallBackFunc Report) // callback progressReport
 {
         ifstream initialFile(source.c_str(), ios::in|ios::binary);
-	ofstream outputFile(dest.c_str(), ios::out|ios::binary);
-	//defines the size of the buffer
-	initialFile.seekg(0, ios::end);
-	long fileSize = initialFile.tellg();
-	//Requests the buffer of the predefined size
-           
-        if(initialFile.is_open() && outputFile.is_open())
-	{         
-		short * buffer = new short[fileSize/2];
-		//Determine the file's size
-		//Then starts from the beginning
-		initialFile.seekg(0, ios::beg);
-		//Then read enough of the file to fill the buffer
-		initialFile.read((char*)buffer, fileSize);
-		//And then write out all that was read
-		outputFile.write((char*)buffer, fileSize);
-		delete[] buffer;
+	
+        if(initialFile.is_open())
+	{   
+            initialFile.seekg(0, ios::end);
+            streampos fileSize = initialFile.tellg();       //get the file size
+            ofstream outputFile(dest.c_str(), ios::out|ios::binary);        //open out stream
+            
+            for(int i=0; i<fileSize; i++){
+                initialFile.seekg(i,ios::beg);
+                char * buffer = new char [1];
+                initialFile.read(buffer,1);
+                outputFile.write(buffer,1);
+                delete[] buffer;
+                Report(i);
+                }
+            
+            outputFile.close();
 	}
-        
-        else if(!initialFile.is_open())
-	{
-		cout<<"I couldn't open "<<source<<" for copying!\n";
+            else 
+            {   cout<<"I couldn't open "<<source<<" for copying!\n";
 		return 0;
-	}
-        
-        else if(!outputFile.is_open())
-	{
-		cout<<"I couldn't open "<<dest<<" for copying!\n";
-		return 0;
-	}
-       
+            }
+ 
 	initialFile.close();
-	outputFile.close();
-
 	return 1;
 }
+
 
 int main(int argc, char* argv[]) {
     
@@ -100,14 +96,16 @@ int main(int argc, char* argv[]) {
                
         }
            result (argc, argv);
-           
+            
            string targetPath(argv[1]);
            for (int i = 2; i<argc; i++) {
                string uploadfile(argv[i]); 
-               string outputPath = targetPath + '/' + uploadfile;  
-               copy(uploadfile , outputPath);
+               string outputPath = targetPath + '/' + uploadfile; 
+               auto fileReport = bind(progressReport,i);
+               copy(uploadfile, outputPath, fileReport);
            }
            
+           cout << "upload is completed.\n";
     }
   
 	return 0;
